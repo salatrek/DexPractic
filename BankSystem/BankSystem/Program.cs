f5e1a460d172598cf7051977126ad95dd318398c
AmberWarder <alexei.270396@gmail.com>
2021-08-05 22:36:14 +0300

    Made changes

--- BankSystem/BankSystem/Program.cs
+++ BankSystem/BankSystem/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BankSystem.Models;
+using BankSystem.Services;
+using System;
 
 namespace BankSystem
 {
@@ -6,7 +8,18 @@ namespace BankSystem
     {
         static void Main(string[] args)
         {
-            
+            var service = new BankService();
+            service.AddPerson(new Client() {Name = "Alex client",Age = 25, PassportID = 580, Status = "Good" });
+            service.AddPerson(new Employee() { Name = "John employee", Age = 34, PassportID = 789, Position = "Clerk" });
+
+            try
+            {
+                service.FindClient(789);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
